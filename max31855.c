78537eb143ecf9195915647487b2a09ebf9ae1ec
Nathan Duprey <nathanduprey@hotmail.com>
2018-04-02 23:08:44 -0400

    added reference read and fault detection

--- max31855.c
+++ max31855.c
@@ -28,17 +28,28 @@ void Max31855Init(volatile uint8_t * port, uint8_t pin){
 }
 
 
-//
+// Description:
+//      Reads the temperature from the attached thermocouple
+// Arguments:
+//      None
+// Returns:
+//      float: The temperature in degrees Celsius (maybe remove float)
+//             9999 if a fault is detected
 float Max31855Read(void){
     int16_t result = 0;
     
     // drive cs low
     *max31855CsPort &= ~(1 << max31855CsPin);
-    // read high and low words
+    // read high word and discard low word
     uint16_t highWord = SpiTransfer16(0x00);
-    uint16_t lowWord = SpiTransfer16(0x00);
+    SpiTransfer16(0x00);
     // drive cs high
     *max31855CsPort |= (1 << max31855CsPin);
+    
+    // check if fault
+    if(highWord & THERMOCOUPLE_FAULT_BIT){
+        return THERMOCOUPLE_FAULT_RETURN;
+    }
     
     // check if negative
     if(highWord & 0x8000){
@@ -50,4 +61,63 @@ float Max31855Read(void){
     
     float num = result * 0.25;
     return num;
+}
+
+
+// Description:
+//      Reads the internal reference junction temperature
+// Arguments:
+//      None
+// Returns:
+//      float: The temperature in degrees Celsius (maybe remove float)
+//             9999 if a fault is detected
+float Max31855ReadReference(void){
+    int16_t result = 0;
+        
+    // drive cs low
+    *max31855CsPort &= ~(1 << max31855CsPin);
+    // discard high word and read low word
+    uint16_t highWord = SpiTransfer16(0x00);
+    uint16_t lowWord = SpiTransfer16(0x00);
+    // drive cs high
+    *max31855CsPort |= (1 << max31855CsPin);
+    
+    // check if fault
+    if(highWord & THERMOCOUPLE_FAULT_BIT){
+        return THERMOCOUPLE_FAULT_RETURN;
+    }
+        
+    // check if negative
+    if(lowWord & 0x8000){
+        // shift over and sign extend
+        result = (lowWord >> 4) | 0xF000;
+    }else{
+        result = lowWord >> 4;
+    }
+    
+    float num = result * 0.0625;
+    return num;
+}
+
+
+// Description:
+//      Reads the fault status
+// Arguments:
+//      None
+// Returns:
+//      uint8_t: The fault bits. Check with definitions in max31855.h
+uint8_t Max31855ReadFault(void){
+    // drive cs low
+    *max31855CsPort &= ~(1 << max31855CsPin);
+    // read high word and read low word
+    uint16_t highWord = SpiTransfer16(0x00);
+    uint16_t lowWord = SpiTransfer16(0x00);
+    // drive cs high
+    *max31855CsPort |= (1 << max31855CsPin);
+    
+    if(!(highWord & THERMOCOUPLE_FAULT_BIT)){
+        return 0;
+    }else{
+        return (lowWord & 0x07);
+    }
 }
--- max31855.h
+++ max31855.h
@@ -9,6 +9,8 @@
 #ifndef MAX31855_H_
 #define MAX31855_H_
 
+#define THERMOCOUPLE_FAULT_RETURN   9999
+#define THERMOCOUPLE_FAULT_BIT  1
 #define THERMOCOUPLE_SHORT_VCC  4
 #define THERMOCOUPLE_SHORT_GND  2
 #define THERMOCOUPLE_OPEN       1
@@ -16,5 +18,7 @@
 
 void Max31855Init(volatile uint8_t * port, uint8_t pin);
 float Max31855Read(void);
+float Max31855ReadReference(void);
+uint8_t Max31855ReadFault(void);
 
 #endif /* MAX31855_H_ */
